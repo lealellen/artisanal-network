00ec50487490028fa359fad47bfa3114b037afb7
lealellen <ellensleal@gmail.com>
2025-05-12 15:14:42 -0300

    tira one hot encoder

--- src/main.py
+++ src/main.py
@@ -25,13 +25,6 @@ X = scaler.fit_transform(X)
 # Dividir dados
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# One-hot encoding dos rótulos
-onehot_encoder = OneHotEncoder(sparse_output=False)
-y_train_onehot = pd.get_dummies(y_train).values
-
-print("Shape de X_train:", X_train.shape)
-print("Shape de y_train_onehot:", y_train_onehot.shape)
-
 # Parâmetros da MLP
 input_size = X_train.shape[1]
 hidden_layers = 5
@@ -40,7 +33,7 @@ output_size = len(np.unique(y))
 mlp = MLP(input_size, hidden_layers, output_size, taxa_aprendizado=0.01, epocas=20000)
 
 print("Iniciando o treinamento...")
-errors = mlp.fit(X_train, y_train_onehot)
+errors = mlp.fit(X_train, y_train)
 
 # Predição
 y_pred_probs = mlp.predict(X_test)
@@ -54,7 +47,7 @@ print(f"Acurácia no conjunto de teste: {acc * 100:.2f}%")
 validacao_cruzada(
     x_treino=X_train,
     k_folds=5,
-    y_treino=y_train_onehot,
+    y_treino=y_train,
     model_params={
         "tamanho_entrada": input_size,
         "camadas_escondidas": hidden_layers,
